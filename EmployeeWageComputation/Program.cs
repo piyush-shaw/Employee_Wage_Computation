ab7543c0d8a5cc4d545c67b03b9e8b3ea25d75a0
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-04 22:42:32 +0530

    calculating employee daily wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,15 +4,20 @@
     {
         Console.WriteLine("Welcome to Employee Wage Computation Program");
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HR = 20;
+        int empHrs = 0;
+        int empWage = 0;
         Random random = new Random();
         int empCheck = random.Next(0, 2);
         if (empCheck == IS_FULL_TIME)
         {
-            Console.WriteLine("Employee is Present");
+            empHrs = 8;
         }
         else
         {
-            Console.WriteLine("Employee is Absent");
+            empHrs = 0;
         }
+        empWage = empHrs * EMP_RATE_PER_HR;
+        Console.WriteLine("Emp Wage : "+empWage);
     }
 }
