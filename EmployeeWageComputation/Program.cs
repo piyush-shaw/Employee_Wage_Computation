fe2e7a9c721bdfcd55244e296b2b7d9128232f20
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-05 11:45:17 +0530

    calculating employee wage of total working hour for a month

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,14 +4,16 @@
     public const int IS_FULL_TIME = 2;
     public const int EMP_RATE_PER_HR = 20;
     public const int NUM_OF_WORKING_DAYS = 20;
+    public const int MAX_HRS_IN_MONTH = 100;
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Employee Wage Computation Program");
         int empHrs = 0;
-        int empWage = 0;
-        int totalEmpWage = 0;
-        for (int day = 0; day<NUM_OF_WORKING_DAYS; day++)
+        int totalWorkingDays = 0;
+        int totalEmpHrs = 0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
+            totalWorkingDays++;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             switch (empCheck)
@@ -26,10 +28,10 @@
                     empHrs = 0;
                     break;
             }
-            empWage = empHrs * EMP_RATE_PER_HR;
-            totalEmpWage = totalEmpWage + empWage;
-            Console.WriteLine("Emp Wage : " + empWage);
+            totalEmpHrs = totalEmpHrs + empHrs;
+            Console.WriteLine("Day# "+totalWorkingDays + " EmpHrs "+ empHrs);
         }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
         Console.WriteLine("Total emp wage :" + totalEmpWage);
     }
 }
