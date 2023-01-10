9050b235575789535900e5cc03de8bdaea79e354
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-10 21:25:23 +0530

    refactor the code to write a class to compute employee wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,9 +5,8 @@
     public const int EMP_RATE_PER_HR = 20;
     public const int NUM_OF_WORKING_DAYS = 20;
     public const int MAX_HRS_IN_MONTH = 100;
-    static void Main(string[] args)
+    public static void computeEmpWage()
     {
-        Console.WriteLine("Welcome to Employee Wage Computation Program");
         int empHrs = 0;
         int totalWorkingDays = 0;
         int totalEmpHrs = 0;
@@ -29,9 +28,14 @@
                     break;
             }
             totalEmpHrs = totalEmpHrs + empHrs;
-            Console.WriteLine("Day# "+totalWorkingDays + " EmpHrs "+ empHrs);
+            Console.WriteLine("Day# " + totalWorkingDays + " EmpHrs " + empHrs);
         }
         int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
         Console.WriteLine("Total emp wage :" + totalEmpWage);
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Employee Wage Computation Program");
+        computeEmpWage();
     }
 }
