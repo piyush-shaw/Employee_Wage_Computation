131e576fa1fda55cbfb065fc05772a73760cea47
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-14 20:35:22 +0530

    [Piyush] added ability to save the total employee wage for each companies

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,43 +1,19 @@
-﻿internal class Program
+﻿using EmployeeWageComputation;
+class Program
 {
-    public const int IS_PART_TIME = 1;
-    public const int IS_FULL_TIME = 2;
-    public const int EMP_RATE_PER_HR = 20;
-    public const int NUM_OF_WORKING_DAYS = 20;
-    public const int MAX_HRS_IN_MONTH = 100;
-    public static void computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-    {
-        int empHrs = 0;
-        int totalWorkingDays = 0;
-        int totalEmpHrs = 0;
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-        {
-            totalWorkingDays++;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
-            {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
-            }
-            totalEmpHrs = totalEmpHrs + empHrs;
-            Console.WriteLine("Day# " + totalWorkingDays + " EmpHrs " + empHrs);
-        }
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
-        Console.WriteLine("Total emp wage for company :"+company +" is " + totalEmpWage);
-
-    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Employee Wage Computation Program");
-        computeEmpWage("Dmart", 30, 24, 80);
-        computeEmpWage("McDonald", 40, 22, 100);
+        EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 25, 22, 80);
+        EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 30, 23, 76);
+        EmpWageBuilderObject bigbasket = new EmpWageBuilderObject("BigBasket", 40, 24, 85);
+        dMart.computeEmpWage();
+        reliance.computeEmpWage();
+        bigbasket.computeEmpWage();
+        Console.WriteLine(dMart.toString());
+        Console.WriteLine(reliance.toString());
+        Console.WriteLine(bigbasket.toString()); ;
     }
+
 }
+   
+    
